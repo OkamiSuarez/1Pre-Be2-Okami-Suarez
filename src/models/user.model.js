32f42386b709cb58d3a8223481bbdf3de0326c2f
OkamiSuarez <okami97.backdev@hotmail.com>
2025-02-28 16:48:00 -0600

    ajustando handlebars para que reciba la data, hay que trabajar con el model mas a fondo

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,9 +2,17 @@ import mongoose from "mongoose";
 
 const usuarioSchema = new mongoose.Schema({
     // ESTO SE MODIFICA CON RELACION A LO QUE PIDE LA CONSIGNA 
-    usuario: String,
+    
+    first_name:String,
+    last_name:String,
+    email:{
+        type:String,
+        unique: true
+    },
+    age:Number,
     password: String,
     cart: String,
+    // usuario: String,
     rol:{
         type:String,
         enum: ["admin","user"],
